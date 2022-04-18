010016e7a47938a54a4dfa8cb28c21f8bed75283
Tw1ck11 <qwentyn2222222222@gmail.com>
2022-04-18 17:51:34 +0200

    Add project files.

---
+++ Слэши и точки.cpp
@@ -0,0 +1,49 @@
+﻿// Слэши и точки.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+cout <<"                                 *                                                                                                                                                                          \n";
+cout <<"                               *   *                                                                                                                                                                         \n";
+cout <<"                             *       *                                                                                                                                                                       \n";
+cout <<"                           *           *        ******                                                                                                                                                             \n";
+cout <<"                         *               *      *    *                                                                                                                                                         \n";
+cout <<"                       *                   *    *    *                                                                                                                                                         \n";
+cout <<"                     *                       *  *    *                                                                                                                                                         \n";
+cout <<"                   *                           *     *                                                                                                                                                        \n";
+cout <<"                 *                              *    *                                                                                                                                                      \n";
+cout <<"               *                                  *  *                                                                                                                                                       \n";
+cout <<"             *                                      *                                                                                                                                                        \n";
+cout <<"           *                                          *                                                                                                                                                      \n";
+cout <<"         *                                              *                                                                                                                                                    \n";
+cout <<"       *                                                  *                                                                                                                                                  \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *    ********             *********                   *                                                                                                                                                \n";
+cout <<"      *    *      *             *       *                   *                                                                                                                                                \n";
+cout <<"      *    *      *             *       *                   *                                                                                                                                                \n";
+cout <<"      *    *      *             *       *                   *                                                                                                                                                \n";
+cout <<"      *    *      *             *       *                   *                                                                                                                                                \n";
+cout <<"      *    *      *             *       *                   *                                                                                                                                                \n";
+cout <<"      *    *      *             *********                   *                                                                                                                                                \n";
+cout <<"      *    ********                                         *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                                                                \n";
+cout <<"      *                                                     *                                                                                                     *    *     *                                  \n";
+cout <<"      *                                                     *               *            *           *          *           *           *           *           *  *  *   * *   *                                         \n";
+cout <<"      *                                                     *             *    *       *   *       *   *      *   *       *   *       *   *       *   *            *   *      *                                               \n";
+cout <<"      *                            ***********              *           *        *   *       *   *       *  *       *   *       *   *       *   *       *        *   *     *  *                                                \n";
+cout <<"      *                            *         *              *         *            *           *          *           *           *           *           *        *  *  *                                              \n";
+cout <<"      *                            *         *              *         *************************************************************************************          *  *    *                                                                                                                      \n";
+cout <<"      *             ********       *         *              *         *                                                                                   *           * *  *                                  \n";
+cout <<"      *             *      *       *         *              *         *                                                                                   *            **                                    \n";
+cout <<"      *             * *    *       *         *              *         *                                                                                   *            **                                    \n";
+cout <<"      *             * *    *       ***********              *         *                                                                                   *            **                                    \n";
+cout <<"      *             * *    *                                *         *                                                                                   *            **                                   \n";
+cout <<"      *             *      *                                *         *                                                                                   *            **                                   \n"
